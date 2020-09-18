0e2581a4907b792ab17c60af7b2bebd38c4ddc4a
Jonas <jonaslagoteixeira@gmail.com>
2020-09-18 17:42:14 -0300

    foi feito o modal

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal__close');
+
+const modalBox = document.querySelector('.modal__box');
+
+modalClose.addEventListener('click', close);
+
+function fadeIn() {
+  TweenMax.to(modal, 0.5, { autoAlpha: 1 });
+  TweenMax.from(modalBox, 0.5, { autoAlpha: 0, top: 300 });
+}
+
+function close() {
+  TweenMax.set([modal, modalBox], { clearProps: 'all' });
+  TweenMax.to(modal, 0.5, { autoAlpha: 0 });
+}
